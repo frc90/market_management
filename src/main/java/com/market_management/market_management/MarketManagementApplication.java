1069be53c8fa0fab0a7289def70f5d90b50544a8
frc90 <39072359+frc90@users.noreply.github.com>
2024-10-05 11:58:13 -0300

    Start project with Hello world!

---
+++ src/main/java/com/market_management/market_management/MarketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.market_management.market_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/market_management/market_management/controllers/GreetingController.java
@@ -0,0 +1,13 @@
+package com.market_management.market_management.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Hello World";
+    }
+}
